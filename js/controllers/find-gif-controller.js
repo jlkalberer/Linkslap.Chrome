51688e6b4a49b2d7f4176207f9a286965b560622
John Kalberer <jlkalberer@gmail.com>
2014-07-23 21:54:42 -0700

    Reverted to gifme search

--- js/controllers/find-gif-controller.js
+++ js/controllers/find-gif-controller.js
@@ -21,6 +21,7 @@
 
 			$analytics.eventTrack('Gif Search', { category: $scope.subscriptionId, label: $scope.search })
 
+			/*
 			rest.oneUrl('search', 'http://api.giphy.com/v1/gifs/search')
 				.get({ api_key: 'dc6zaTOxFJmzC', q: $scope.search, offset: ($scope.currentPage - 1) * $scope.limit, limit: $scope.limit })
 				.then(function (result) {
@@ -40,8 +41,41 @@
 				    	}
 				    }
 
+				    if (group.length) {
+				    	$scope.results.push(group);
+				    }
+
 					$scope.searching = false;
 				});
+			*/
+
+			rest.oneUrl('search', 'http://api.gifme.io/v1/search')
+				.get({ key: 'MNfCaCC9tRAr3yzf', query: $scope.search, page: ($scope.currentPage - 1), limit: $scope.limit, sfw: !$scope.nsfw })
+				.then(function (result) {
+					var pagination = result.meta;
+					$scope.totalCount = pagination.total;
+					$scope.pageCount =  Math.ceil(pagination.total / $scope.limit);
+					var allItems = result.data;
+
+					$scope.results = [];
+				    var group = [];
+
+				    for (var i = 0; i < allItems.length; i += 1) {
+				    	group.push(allItems[i]);
+				    	if ((i + 1) % 2 === 0) {
+				    		$scope.results.push(group);
+				    		group = [];
+				    	}
+				    }
+
+				    if (group.length) {
+				    	$scope.results.push(group);
+				    }
+
+					$scope.searching = false;
+				});
+			
+			
 		};
 
 		$scope.$watch('search', function () { $scope.currentPage = 1; $scope.searchGif(); });
